0d7e5e9c0efb946c9e6c99ff51b0f6f5e825183b
Diego González Suárez <UO276406@uniovi.es>
2024-12-20 20:01:22 +0100

    Añadido doble interes

--- src/Ejercicio2.jsx
+++ src/Ejercicio2.jsx
@@ -29,7 +29,7 @@ export function Ejercicio2() {
       const signer = provider.getSigner();
 
       bank.current = new Contract(
-        "0xae4D79cD17294d8e180bE5325a58c64E40a2EF7E",
+        "0x722f60cF1040C42ba0C3a5DfeA9FD8828E4d82B1",
         bankManifest.abi,
         signer
       );
@@ -37,29 +37,46 @@ export function Ejercicio2() {
     return null;
   };
 
-  let onSubmitDeposit = async (e) => {
+  let onSubmitDeposit = async (e, double) => {
     e.preventDefault();
 
     const BNBamount = parseFloat(e.target.elements[0].value);
 
     // Wei to BNB se pasa con ethers.utils recibe un String!!!
-    const tx = await bank.current.deposit({
-      value: ethers.utils.parseEther(String(BNBamount)),
-      gasLimit: 6721975,
-      gasPrice: 20000000000,
-    });
+    if (double) {
+      const tx = await bank.current.depositDouble({
+        value: ethers.utils.parseEther(String(BNBamount)),
+        gasLimit: 6721975,
+        gasPrice: 20000000000,
+      });
+      await tx.wait();
+    } else {
+      const tx = await bank.current.deposit({
+        value: ethers.utils.parseEther(String(BNBamount)),
+        gasLimit: 6721975,
+        gasPrice: 20000000000,
+      });
+      await tx.wait();
+    }
 
-    await tx.wait();
     await getBalance();
     await getInterest();
   };
 
-  let clickWithdraw = async (e) => {
-    await await bank.current.withdraw({
-      value: ethers.utils.parseEther(String(0.05)),
-      gasLimit: 6721975,
-      gasPrice: 20000000000,
-    });
+  let clickWithdraw = async (e, double) => {
+    if (double) {
+      await bank.current.withdrawDouble({
+        value: ethers.utils.parseEther(String(0.1)),
+        gasLimit: 6721975,
+        gasPrice: 20000000000,
+      });
+    } else {
+      await bank.current.withdraw({
+        value: ethers.utils.parseEther(String(0.1)),
+        gasLimit: 6721975,
+        gasPrice: 20000000000,
+      });
+    }
   };
 
   const getBalance = async () => {
@@ -92,12 +109,22 @@ export function Ejercicio2() {
   return (
     <div>
       <h1>Bank</h1>
-      <h2>Deposit</h2>
-      <form onSubmit={(e) => onSubmitDeposit(e)}>
+      <h2>Deposit (Basic)</h2>
+      <form onSubmit={(e) => onSubmitDeposit(e, false)}>
         <input type="number" step="0.01" />
         <button type="submit">Deposit</button>
       </form>
-      <button onClick={() => clickWithdraw()}> Withdraw </button>
+      <button onClick={(e) => clickWithdraw(e, false)}> Withdraw </button>
+      <h2>Deposit (Double): It will be blocked during 10 minutes</h2>
+      <form onSubmit={(e) => onSubmitDeposit(e, true)}>
+        <input type="number" step="0.01" />
+        <button type="submit">Deposit</button>
+      </form>
+      <button onClick={(e) => clickWithdraw(e, true)}>
+        {" "}
+        Withdraw (Double){" "}
+      </button>
+      <br />
       <h2>Balance: {balance} BNB</h2>
       <h2>Interest: {interest} BMIW</h2>
       <button onClick={() => update()}>Update balance and interes</button>
